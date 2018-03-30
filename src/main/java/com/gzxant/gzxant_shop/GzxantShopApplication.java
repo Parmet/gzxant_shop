5cb5cacad2fbc4f90eb2f5603108a6eda03d7d52
xiaoyuchuan <158215350@qq.com>
2018-03-30 15:41:18 +0800

    init

---
+++ src/main/java/com/gzxant/gzxant_shop/GzxantShopApplication.java
@@ -0,0 +1,12 @@
+package com.gzxant.gzxant_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzxantShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GzxantShopApplication.class, args);
+	}
+}
